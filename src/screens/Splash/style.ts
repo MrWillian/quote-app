01223ed6c276e81b9794f7c8b0298bbe09c3bf98
MrWillian <willian.dev10@gmail.com>
2023-03-27 20:50:30 -0300

    refactor(screens): remove Logo styled-component

--- src/screens/Splash/style.ts
+++ src/screens/Splash/style.ts
@@ -7,11 +7,6 @@ export const Container = styled.View`
   background-color: #292b38;
 `;
 
-export const Logo = styled.Image`
-  margin-top: 20px;
-  margin-bottom: 20px;
-`;
-
 export const Title = styled.Text`
   color: #5c5091;
   font-weight: bold;
