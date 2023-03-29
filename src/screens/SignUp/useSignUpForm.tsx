9862430f14f3dd244cb38b9387de0dae94b99760
MrWillian <willian.dev10@gmail.com>
2023-03-29 16:06:48 -0300

    refactor(useForm): add custom useForm to SignUp

---
+++ src/screens/SignUp/useSignUpForm.tsx
@@ -0,0 +1,38 @@
+import {useForm} from 'react-hook-form';
+import {yupResolver} from '@hookform/resolvers/yup';
+import {object, ref, string} from 'yup';
+
+export type UserSignUpProps = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+let schema = object().shape({
+  givenName: string()
+    .required('O nome é obrigatório!')
+    .min(3, 'O nome deve conter pelo menos 3 caracteres!'),
+  email: string()
+    .required('O email é obrigatório!')
+    .email('Digite um email válido'),
+  password: string()
+    .required('A senha é obrigatória')
+    .matches(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#.\$%\^&\*])(?=.{8,})/,
+      'A senha deverá conter pelo menos 8 caracteres',
+    ),
+  confirmPassword: string().oneOf([ref('password')], 'As senhas não conferem!'),
+});
+
+export const useSignUpForm = () =>
+  useForm<UserSignUpProps>({
+    mode: 'all',
+    resolver: yupResolver(schema),
+    defaultValues: {
+      name: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+    },
+  });
