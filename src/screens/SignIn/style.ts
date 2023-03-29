0f6a9fe87984702df603c07264fcd23cd4f1b089
MrWillian <willian.dev10@gmail.com>
2023-03-28 21:40:54 -0300

    feat(screens): add SignIn styles

---
+++ src/screens/SignIn/style.ts
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #292b38;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 24px;
+  text-transform: uppercase;
+  letter-spacing: 8px;
+`;
+
+export const Form = styled.SafeAreaView`
+  display: flex;
+  margin-top: 20px;
+  gap: 4px;
+`;
