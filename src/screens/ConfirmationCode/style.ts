4b9200c445d56d0a6fc80a223d3a94c5935e927e
MrWillian <willian.dev10@gmail.com>
2023-03-30 21:32:48 -0300

    feat(screens): add ConfirmationCode style

---
+++ src/screens/ConfirmationCode/style.ts
@@ -0,0 +1,49 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #292b38;
+  padding: 5px;
+`;
+
+export const Title = styled.Text`
+  color: #fff;
+  font-weight: bold;
+  font-size: 24px;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  text-align: center;
+`;
+
+export const Subtitle = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  text-align: center;
+  margin-top: 5px;
+`;
+
+export const EmailLabel = styled.Text`
+  color: #fff;
+  font-size: 20px;
+  text-decoration: underline;
+  letter-spacing: 4px;
+  text-align: center;
+`;
+
+export const Form = styled.SafeAreaView`
+  display: flex;
+  margin-top: 20px;
+  gap: 20px;
+`;
+
+export const Inputs = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  gap: 5px;
+`;
