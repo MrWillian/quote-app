4b73b6e55f7de1cec0b46d66441207ac5e01b367
MrWillian <willian.dev10@gmail.com>
2023-04-02 09:43:34 -0300

    feat(components): add QuoteList component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export {QuoteLogo} from './QuoteLogo';
 export {TextInput} from './TextInput';
 export {Button} from './Button';
+export {SearchBar} from './SearchBar';
+export {QuoteList} from './QuoteList';
