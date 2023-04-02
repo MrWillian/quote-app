72b606efd8bd83d8b8db90876a8124c46f235220
MrWillian <willian.dev10@gmail.com>
2023-04-02 09:44:01 -0300

    feat(screens): add Dashboard screen

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -2,3 +2,4 @@ export {Splash} from './Splash';
 export {SignIn} from './SignIn';
 export {SignUp} from './SignUp';
 export {ConfirmationCode} from './ConfirmationCode';
+export {Dashboard} from './Dashboard';
