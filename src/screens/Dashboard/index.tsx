609e9a6de14d9add672be6fef80f6922f2789722
MrWillian <willian.dev10@gmail.com>
2023-04-12 11:02:34 -0300

    refactor(screens): handle token storage on Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import {Button, QuoteList, SearchBar} from '../../components';
 import {
   Container,
@@ -7,17 +8,30 @@ import {
   Subtitle,
   Title,
 } from './style';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import {mainScreenProp} from '../../routes/MainStack';
 
 export const Dashboard = () => {
+  const navigation = useNavigation<mainScreenProp>();
+
+  useEffect(() => {
+    handleToken();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const handleToken = async () => {
+    const token = await AsyncStorage.getItem('REFRESH_TOKEN');
+    if (!token) {
+      navigation.goBack();
+    }
+  };
+
   return (
     <Container>
       <Title>Hey!!</Title>
       <Subtitle>Did You Forget Something?</Subtitle>
-
       <SearchBar />
-
       <QuoteList />
-
       <RegisterContainer>
         <RegisterLabel>Register Before You Forget! ;)</RegisterLabel>
         <Button title="Register" />
