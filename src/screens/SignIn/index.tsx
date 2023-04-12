636000c1dbab70987fd4a3ee992ae7464d94656f
MrWillian <willian.dev10@gmail.com>
2023-04-12 10:13:56 -0300

    feat(screens): implement aws cognito sign in feature

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,7 +1,11 @@
 import React, {useEffect} from 'react';
+import {useNavigation} from '@react-navigation/native';
 import {Button, QuoteLogo, TextInput} from '../../components';
 import {Container, Form, Inputs, Title} from './style';
 import {useSignInForm} from './useSignInForm';
+import useAuth from '../../hooks/useAuth';
+import {mainScreenProp} from '../../routes/MainStack';
+import {Alert} from 'react-native';
 
 export const SignIn = () => {
   const {
@@ -10,19 +14,30 @@ export const SignIn = () => {
     handleSubmit,
     formState: {errors},
   } = useSignInForm();
+  const {signIn} = useAuth();
+  const navigation = useNavigation<mainScreenProp>();
 
   useEffect(() => {
     register('email');
     register('password');
   }, [register]);
 
-  const onSubmit = (data: any) => console.log(data);
+  const onSubmit = async (data: any) => {
+    await signIn(data)
+      .then(response => {
+        Alert.alert('Success', response.message);
+        navigation.navigate('Dashboard');
+      })
+      .catch(error => {
+        Alert.alert('Error', error.message);
+      });
+  };
 
   return (
     <Container>
       <QuoteLogo />
       <Title>Hey, Hello!!</Title>
-      <Form onSubmit={handleSubmit(onSubmit)}>
+      <Form>
         <Inputs>
           <TextInput
             id="email"
@@ -38,7 +53,7 @@ export const SignIn = () => {
             onChangeText={text => setValue('password', text)}
           />
         </Inputs>
-        <Button label="Sign In" />
+        <Button onPress={handleSubmit(onSubmit)} title="Sign In" />
       </Form>
     </Container>
   );
