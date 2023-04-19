ba879f435a043e714ecf54a2655338071c76c120
MrWillian <willian.dev10@gmail.com>
2023-04-19 16:05:46 -0300

    refactor(hooks): add index file

---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+import useAuth from './useAuth';
+import useAuthenticatedUser from './useAuthenticatedUser';
+import useLogout from './useLogout';
+
+export {useAuth, useAuthenticatedUser, useLogout};
