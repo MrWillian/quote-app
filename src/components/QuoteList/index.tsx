be2d89dad83d1bb7fdb3e464ceb14be2fd99a083
MrWillian <willian.dev10@gmail.com>
2023-04-19 15:43:44 -0300

    refactor(components): adapt QuoteList to display quotes

--- src/components/QuoteList/index.tsx
+++ src/components/QuoteList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Text} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   Container,
@@ -9,40 +10,37 @@ import {
   QuoteTitle,
 } from './styles';
 
-export const QuoteList = () => (
-  <Container>
-    <Quote>
-      <QuoteInfo>
-        <QuoteTitle>Chave de Fenda</QuoteTitle>
-        <QuoteDescription>
-          Eu coloquei a chave de fenda no ar...
-        </QuoteDescription>
-      </QuoteInfo>
-      <DetailButton>
-        <Icon name="arrow-circle-right" size={24} color="#fff" />
-      </DetailButton>
-    </Quote>
-    <Quote>
-      <QuoteInfo>
-        <QuoteTitle>Chave da moto</QuoteTitle>
-        <QuoteDescription>
-          Eu coloquei a chave da moto na ga...
-        </QuoteDescription>
-      </QuoteInfo>
-      <DetailButton>
-        <Icon name="arrow-circle-right" size={24} color="#fff" />
-      </DetailButton>
-    </Quote>
-    <Quote>
-      <QuoteInfo>
-        <QuoteTitle>Documentação do carro</QuoteTitle>
-        <QuoteDescription>
-          Eu coloquei todos os documentos do carro...
-        </QuoteDescription>
-      </QuoteInfo>
-      <DetailButton>
-        <Icon name="arrow-circle-right" size={24} color="#fff" />
-      </DetailButton>
-    </Quote>
-  </Container>
-);
+type Quote = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+};
+
+type Quotes = {
+  quotes: Quote[];
+};
+
+export const QuoteList = ({quotes}: Quotes) => {
+  return (
+    <Container>
+      {quotes.length > 0 ? (
+        quotes.map(quote => {
+          return (
+            <Quote key={quote.id}>
+              <QuoteInfo>
+                <QuoteTitle>{quote.title}</QuoteTitle>
+                <QuoteDescription>{quote.description}</QuoteDescription>
+              </QuoteInfo>
+              <DetailButton>
+                <Icon name="arrow-circle-right" size={24} color="#fff" />
+              </DetailButton>
+            </Quote>
+          );
+        })
+      ) : (
+        <Text>Nenhum encontrado...</Text>
+      )}
+    </Container>
+  );
+};
