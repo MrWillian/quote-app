e2c7cf5093e0e87bc3d7fa14fb8a46990ce77a5b
MrWillian <willian.dev10@gmail.com>
2023-04-20 09:47:27 -0300

    feat(hooks): add useCurrentUser

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,6 @@
 import useAuth from './useAuth';
 import useAuthenticatedUser from './useAuthenticatedUser';
+import useCurrentUser from './useCurrentUser';
 import useLogout from './useLogout';
 
-export {useAuth, useAuthenticatedUser, useLogout};
+export {useAuth, useAuthenticatedUser, useCurrentUser, useLogout};
