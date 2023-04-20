4b695ae6130848c68af22ebe3aaed4e0ff8fc468
MrWillian <willian.dev10@gmail.com>
2023-04-20 11:45:02 -0300

    feat(hooks): add useDebounce

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,6 +1,7 @@
 import useAuth from './useAuth';
 import useAuthenticatedUser from './useAuthenticatedUser';
 import useCurrentUser from './useCurrentUser';
+import useDebounce from './useDebounce';
 import useLogout from './useLogout';
 
-export {useAuth, useAuthenticatedUser, useCurrentUser, useLogout};
+export {useAuth, useAuthenticatedUser, useCurrentUser, useDebounce, useLogout};
