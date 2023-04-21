04360d6c9d658b9775d029021d13c592b1542c20
MrWillian <willian.dev10@gmail.com>
2023-04-21 15:26:56 -0300

    refactor(screens): remove list of quotes by the Dashboard

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {Button, LogOutButton, QuoteList, SearchBar} from '../../components';
 import {
@@ -10,37 +10,15 @@ import {
   Header,
 } from './style';
 import {mainScreenProp} from '../../routes/MainStack';
-import {getQuotesList} from '../../lib/quotes/listQuotes';
-import {Alert} from 'react-native';
-import {useCurrentUser, useLogout} from '../../hooks';
+import {useLogout} from '../../hooks';
 
 export const Dashboard = () => {
-  const [quotes, setQuotes] = useState([]);
   const navigation = useNavigation<mainScreenProp>();
-  const [currentUser] = useCurrentUser();
   const [logout] = useLogout();
-
-  useEffect(() => {
-    getQuotes();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
 
   const redirectToRegisterQuote = () => navigation.navigate('RegisterQuote');
 
   const handleLogout = () => logout();
-
-  const getQuotes = async () => {
-    const username = currentUser?.getUsername() ?? '';
-    await getQuotesList(username)
-      .then(result => {
-        if (result.data) {
-          setQuotes(result.data.Items);
-        }
-      })
-      .catch(error => {
-        Alert.alert('Error', error.errorMessage);
-      });
-  };
 
   return (
     <Container>
@@ -50,7 +28,7 @@ export const Dashboard = () => {
       </Header>
       <Subtitle>Did You Forget Something?</Subtitle>
       <SearchBar />
-      <QuoteList quotes={quotes} />
+      <QuoteList />
       <RegisterContainer>
         <RegisterLabel>Register Before You Forget! ;)</RegisterLabel>
         <Button title="Register" onPress={redirectToRegisterQuote} />
