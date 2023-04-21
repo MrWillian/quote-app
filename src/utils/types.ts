06308d3a0f1c3726539b58b1987d46aa882acd75
MrWillian <willian.dev10@gmail.com>
2023-04-21 15:27:46 -0300

    refactor(utils): add Quote and Quotes type

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,13 @@
 export const ACCESS_TOKEN = 'ACCESS_TOKEN';
 export const UNVERIFIED_ACCOUNT_EMAIL = 'UNVERIFIED_ACCOUNT_EMAIL';
+
+export type Quote = {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+};
+
+export type Quotes = {
+  quotes: Quote[];
+};
