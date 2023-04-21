9a5f865be7851a603bbc49595b93ce317c5df7b4
MrWillian <willian.dev10@gmail.com>
2023-04-21 15:23:43 -0300

    refactor(hooks): remove useCurrentUser

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,7 +1,6 @@
 import useAuth from './useAuth';
 import useAuthenticatedUser from './useAuthenticatedUser';
-import useCurrentUser from './useCurrentUser';
 import useDebounce from './useDebounce';
 import useLogout from './useLogout';
 
-export {useAuth, useAuthenticatedUser, useCurrentUser, useDebounce, useLogout};
+export {useAuth, useAuthenticatedUser, useDebounce, useLogout};
