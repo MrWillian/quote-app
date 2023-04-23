b82f4b60f50e118d053a6449128f3722a78a4b9b
MrWillian <willian.dev10@gmail.com>
2023-04-22 22:04:18 -0300

    refactor(components): navigate to DetailQuote

--- src/components/QuoteList/index.tsx
+++ src/components/QuoteList/index.tsx
@@ -3,36 +3,45 @@ import Icon from 'react-native-vector-icons/FontAwesome';
 import {
   Container,
   DetailButton,
-  Quote,
+  QuoteItem,
   QuoteDescription,
   QuoteInfo,
   QuoteTitle,
   NotFoundLabel,
 } from './styles';
 import useQuotes from '../../hooks/useQuotes';
+import {useNavigation} from '@react-navigation/native';
+import {mainScreenProp} from '../../routes/MainStack';
+import {Quote} from '../../utils/types';
 
 export const QuoteList = () => {
   const {quotes, listQuotes} = useQuotes();
+  const navigation = useNavigation<mainScreenProp>();
 
   useEffect(() => {
     listQuotes();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const handleDetailQuote = (quote: Quote) => {
+    console.log('selected quote', quote);
+    navigation.navigate('DetailQuote', {quote});
+  };
+
   return (
     <Container>
       {quotes.length > 0 ? (
         quotes.map(quote => {
           return (
-            <Quote key={quote.id}>
+            <QuoteItem key={quote.id}>
               <QuoteInfo>
                 <QuoteTitle>{quote.title}</QuoteTitle>
                 <QuoteDescription>{quote.description}</QuoteDescription>
               </QuoteInfo>
-              <DetailButton>
+              <DetailButton onPress={() => handleDetailQuote(quote)}>
                 <Icon name="arrow-circle-right" size={24} color="#fff" />
               </DetailButton>
-            </Quote>
+            </QuoteItem>
           );
         })
       ) : (
