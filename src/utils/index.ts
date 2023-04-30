27ae15fd207a92f8c139120bb28a460a255d4cfd
MrWillian <willian.dev10@gmail.com>
2023-04-29 23:55:37 -0300

    refactor(utils): export titleAndDescriptionQuote check util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,12 +1,16 @@
 import {storeData, removeData, retrieveData} from './storage';
 import {ACCESS_TOKEN, UNVERIFIED_ACCOUNT_EMAIL} from './types';
 import {cognitoPool} from './cognito-pool';
+import titleAndDescriptionQuoteIncludesFilter from './titleAndDescriptionQuoteIncludesFilter';
+import {Quote} from './types';
 
 export {
   storeData,
   removeData,
   retrieveData,
+  titleAndDescriptionQuoteIncludesFilter,
   cognitoPool,
   ACCESS_TOKEN,
   UNVERIFIED_ACCOUNT_EMAIL,
 };
+export type {Quote};
