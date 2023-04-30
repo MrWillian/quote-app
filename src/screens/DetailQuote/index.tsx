3244fd6544e3c55b206a37b12024785d5acce403
MrWillian <willian.dev10@gmail.com>
2023-04-29 23:44:25 -0300

    refactor(screens): get selected Quote

--- src/screens/DetailQuote/index.tsx
+++ src/screens/DetailQuote/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button} from '../../components';
 import {
   Container,
@@ -15,31 +15,31 @@ import {
 import {useNavigation} from '@react-navigation/native';
 import {mainScreenProp} from '../../routes/MainStack';
 import {Quote} from '../../utils/types';
+import useQuotes from '../../hooks/useQuotes';
 
-type Props = {
-  quote?: Quote;
-};
-
-export const DetailQuote = ({quote}: Props) => {
+export const DetailQuote = () => {
+  const {getSelectedQuote} = useQuotes();
+  const [quote, setQuote] = useState<Quote>();
   const navigation = useNavigation<mainScreenProp>();
 
-  const handleNavigateToDashboard = () => navigation.navigate('Dashboard');
+  useEffect(() => {
+    const auxilliaryQuote = getSelectedQuote();
+    setQuote(auxilliaryQuote);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  console.log('quote', quote);
+  const handleNavigateToDashboard = () => navigation.navigate('Dashboard');
 
   return (
     <Container>
       <Title>Look!!</Title>
       <Subtitle>This Should Help you Remember...</Subtitle>
-
       <ContentContainer>
         <ContentContainerHead>
-          <QuoteTitle>Chave de Fenda</QuoteTitle>
-          <QuoteDate>18/02/2023</QuoteDate>
+          <QuoteTitle>Title: {quote?.title}</QuoteTitle>
+          <QuoteDate>Date: {quote?.date}</QuoteDate>
         </ContentContainerHead>
-        <QuoteDescription>
-          Eu coloquei a chave de fenda no armário porque na caixa não cabia mais
-        </QuoteDescription>
+        <QuoteDescription>Description: {quote?.description}</QuoteDescription>
         <QuoteDeleteButton onPress={() => console.log('Delete')}>
           <QuoteDeleteButtonLabel>Delete</QuoteDeleteButtonLabel>
         </QuoteDeleteButton>
