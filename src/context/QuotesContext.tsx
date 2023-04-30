5e32ff6f81163596d8bbb7205446a2148b876529
MrWillian <willian.dev10@gmail.com>
2023-04-29 23:58:44 -0300

    refactor(context): extract context types

--- src/context/QuotesContext.tsx
+++ src/context/QuotesContext.tsx
@@ -3,28 +3,7 @@ import React, {useState, useCallback} from 'react';
 import {getQuotesList} from '../lib/quotes/listQuotes';
 import {useAuthenticatedUser} from '../hooks';
 import {Quote, titleAndDescriptionQuoteIncludesFilter} from '../utils';
-
-export interface IQuotesContextType {
-  quotes: Quote[];
-  selectedQuote?: Quote;
-  listQuotes: () => Promise<unknown>;
-  getSelectedQuote: () => Quote | undefined;
-  selectQuote: (quote: Quote) => void;
-  filterQuotes: (filter: string) => void;
-}
-
-export const QuotesContext = React.createContext<IQuotesContextType>({
-  quotes: [],
-  selectedQuote: undefined,
-  listQuotes: async () => null,
-  getSelectedQuote: () => undefined,
-  selectQuote: () => null,
-  filterQuotes: () => null,
-});
-
-export interface IQuotesProviderProps {
-  children?: React.ReactNode;
-}
+import {IQuotesProviderProps, QuotesContext} from './types/quotes';
 
 export const QuotesProvider = ({children}: IQuotesProviderProps) => {
   const [quotes, setQuotes] = useState<Quote[]>([]);
