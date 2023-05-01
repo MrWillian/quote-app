84d4648f2becc0f6b2aed8a6ab8ae7a11803bec6
MrWillian <willian.dev10@gmail.com>
2023-05-01 14:27:42 -0300

    feat(utils): add util function to sanitize quote data to save

---
+++ src/utils/sanitizeQuoteDataToSave.ts
@@ -0,0 +1,13 @@
+import uuid from 'react-native-uuid';
+
+const sanitizeQuoteDataToSave = (payload, sub: string) => ({
+  id: uuid.v4(),
+  title: payload.title,
+  description: payload.description,
+  user_id: sub,
+  date: new Date(payload.date).toLocaleString().split(',')[0],
+  created_at: new Date(),
+  updated_at: null,
+});
+
+export default sanitizeQuoteDataToSave;
