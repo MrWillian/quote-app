95e8524f79b89c7b976c63ff67f28c752d8d1dd6
MrWillian <willian.dev10@gmail.com>
2023-05-02 12:02:06 -0300

    feat(screens): translate SignIn form

--- src/screens/SignUp/useSignUpForm.tsx
+++ src/screens/SignUp/useSignUpForm.tsx
@@ -1,6 +1,7 @@
 import {useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import {object, ref, string} from 'yup';
+import i18n from 'i18next';
 
 export type UserSignUpProps = {
   givenName: string;
@@ -11,21 +12,21 @@ export type UserSignUpProps = {
 
 let schema = object().shape({
   givenName: string()
-    .required('O nome é obrigatório!')
-    .min(3, 'O nome deve conter pelo menos 3 caracteres!'),
+    .required(i18n.t('required_name') ?? '')
+    .min(3, i18n.t('minimum_characters_name') ?? ''),
   email: string()
-    .required('O email é obrigatório!')
-    .email('Digite um email válido'),
+    .required(i18n.t('required_email') ?? '')
+    .email(i18n.t('valid_email') ?? ''),
   password: string()
-    .required('A senha é obrigatória')
+    .required(i18n.t('required_password') ?? '')
     .matches(
       /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#.$%^&*])(?=.{8,})/,
-      `The password is required and must contain at least 8 characters: 
-      * a number
-      * a special character (!@#$%^&.*)
-      * a uppercase latter and a lowercase latter`,
+      i18n.t('valid_password') ?? '',
     ),
-  confirmPassword: string().oneOf([ref('password')], 'As senhas não conferem!'),
+  confirmPassword: string().oneOf(
+    [ref('password')],
+    i18n.t('valid_confirm_password') ?? '',
+  ),
 });
 
 export const useSignUpForm = () =>
