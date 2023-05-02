0032ffebbac461036a32b7ac84ea3a85f51a2c98
MrWillian <willian.dev10@gmail.com>
2023-05-02 14:44:14 -0300

    feat(translation): set the current locale to get language

--- src/translation/index.ts
+++ src/translation/index.ts
@@ -1,6 +1,12 @@
 import i18n from 'i18next';
+import {NativeModules, Platform} from 'react-native';
 import {initReactI18next} from 'react-i18next';
 import * as resources from './resources';
+
+const locale =
+  Platform.OS === 'ios'
+    ? NativeModules.SettingsManager.settings.AppleLocale
+    : NativeModules.I18nManager.localeIdentifier;
 
 i18n.use(initReactI18next).init({
   compatibilityJSON: 'v3',
@@ -15,5 +21,5 @@ i18n.use(initReactI18next).init({
       {},
     ),
   },
-  lng: 'pt',
+  lng: locale.substring(0, 2),
 });
