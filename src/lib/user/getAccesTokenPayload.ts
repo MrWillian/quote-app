a225b6065fe2746077c4530d5f98fdddc5e40047
MrWillian <willian.dev10@gmail.com>
2023-05-05 21:31:52 -0300

    refactor(lib): remove unnecessary comment

--- src/lib/user/getAccesTokenPayload.ts
+++ src/lib/user/getAccesTokenPayload.ts
@@ -1,12 +1,12 @@
 import {CognitoJwtVerifier} from 'aws-jwt-verify';
 import {CLIENT_ID, USER_POOL_ID} from '../../../env-vars';
 
-// Verifier that expects valid access tokens:
 const verifier = CognitoJwtVerifier.create({
   userPoolId: USER_POOL_ID,
   tokenUse: 'access',
   clientId: CLIENT_ID,
 });
+
 const getPayload = async (accessToken: string) => {
   try {
     const payload = await verifier.verify(accessToken);
