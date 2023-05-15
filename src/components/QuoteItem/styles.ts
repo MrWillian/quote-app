0af245c919247ea8a4ed772f6ea4916ec81da3dc
MrWillian <willian.dev10@gmail.com>
2023-05-15 15:34:19 -0300

    feat(components): add QuoteItem

---
+++ src/components/QuoteItem/styles.ts
@@ -0,0 +1,24 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin: 2px;
+  padding: 5px 10px;
+`;
+
+export const QuoteInfo = styled.View``;
+
+export const QuoteTitle = styled.Text`
+  color: #fff;
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const QuoteDescription = styled.Text`
+  color: #fff;
+`;
+
+export const DetailButton = styled.TouchableOpacity``;
